0c801faec92b55940c691055127ece7e2a10d378
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-04-10 22:20:06 +0800

    1st commit

---
+++ js/initSimpleLoading.js
@@ -0,0 +1,20 @@
+/*
+ * UTILITY | simple loading v1.0.0
+ * build: 20.01.25 @17:38
+ */
+
+export function initSimpleLoading() {
+    const loader = document.querySelector('[data-id="load"]');
+    if (!loader) return;
+  
+    gsap.to(loader, {
+      opacity: 0,
+      delay: 0.35,
+      duration: 1,
+      ease: "power2.out",
+      onComplete() {
+        loader.style.display = "none";
+      }
+    });
+  }
+  
---
+++ js/main.js
@@ -0,0 +1,27 @@
+/**
+ * MAUDE VON GIESE | main modules
+ * @build 22.09.25
+ * @updated 16:39 PHT
+ * @author TONYTONY Sàrl
+ */
+
+console.log(
+    "%c🦄 Deploying main modules",
+    "color: white; background: purple; padding: 2px 6px; border-radius: 3px;",
+);
+
+import { initSimpleLoading } from 'https://cdn.maudvongiese.ch/js/initSimpleLoading.js?v=1.0.0';
+
+
+// on DOM loaded
+document.addEventListener("DOMContentLoaded", () => {
+    // critical
+    initSimpleLoading();
+
+    // font-dependent
+    document.fonts.ready.then(() => {
+        console.log(`✅ Initialized all font-dependent modules`);
+    });
+
+    console.log(`✅ Initialized all DOM-dependent modules`);
+});
