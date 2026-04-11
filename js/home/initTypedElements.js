b8ca29f9bc03d89eb0b82317f86cf701f25cd5ec
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-04-11 11:15:20 +0800

    Update initTypedElements.js

--- js/home/initTypedElements.js
+++ js/home/initTypedElements.js
@@ -2,32 +2,32 @@
  * UTILITY | typed elements v1.1 (ES6 module)
  */
 
+import Typed from "https://cdn.jsdelivr.net/npm/typed.js@2.0.16/dist/typed.esm.js";
+
 export function initTypedElements() {
-    const OPTIONS = {
-      TARGETS: document.querySelectorAll('[data-id="info-bottom"]'),
-      TOTAL_DURATION: 850 // ms
-    };
-  
-    // 🥭 breakpoint check
-    if (window.innerWidth < 992) {
-      console.log("skipping typed elements");
-      return;
-    }
-  
-    // 🥭 init
-    OPTIONS.TARGETS.forEach((el) => {
-      const text = el.textContent.trim();
-      if (!text.length) return;
-  
-      el.textContent = "";
-  
-      const typeSpeed = OPTIONS.TOTAL_DURATION / text.length;
-  
-      new Typed(el, {
-        strings: [text],
-        typeSpeed,
-        backSpeed: 25,
-        showCursor: false
-      });
-    });
+  const OPTIONS = {
+    TARGETS: document.querySelectorAll('[data-id="info-bottom"]'),
+    TOTAL_DURATION: 850
+  };
+
+  if (window.innerWidth < 992) {
+    console.log("skipping typed elements");
+    return;
   }
+
+  OPTIONS.TARGETS.forEach((el) => {
+    const text = el.textContent.trim();
+    if (!text.length) return;
+
+    el.textContent = "";
+
+    const typeSpeed = OPTIONS.TOTAL_DURATION / text.length;
+
+    new Typed(el, {
+      strings: [text],
+      typeSpeed,
+      backSpeed: 25,
+      showCursor: false
+    });
+  });
+}
